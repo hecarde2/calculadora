448803df999a3dd6e540a02357c37a37ae6af273
Hector Mario Carvajal de los Reyes <hectordico@hotmail.com>
2026-04-21 18:07:26 -0500

    cmabios

--- script.js
+++ script.js
@@ -1,44 +1,83 @@
-let num1 = "";
-let num2 = "";
-let operador = "";
+let numero1 = "";
+let numero2 = "";
+let signo = "";
+
 
 function agregar(valor) {
 
-    if (valor === "+" || valor === "-" || valor === "*" || valor === "/") {
-        operador = valor;
-        document.getElementById("pantalla").value += valor;
-    } else {
-        if (operador === "") {
-            num1 += valor;
-        } else {
-            num2 += valor;
-        }
-        document.getElementById("pantalla").value += valor;
+   
+    if (valor === "+") {
+        signo = "+";
     }
+    
+    else if (valor === "-") {
+        signo = "-";
+    }
+   
+    else if (valor === "*") {
+        signo = "*";
+    }
+   
+    else if (valor === "/") {
+        signo = "/";
+    }
+    
+    else {
+
+        
+        if (signo === "") {
+            numero1 = numero1 + valor;
+        }
+        
+        else {
+            numero2 = numero2 + valor;
+        }
+    }
+
+    
+    mostrar();
 }
+
+
+function mostrar() {
+    document.getElementById("p1").textContent = numero1;
+    document.getElementById("p2").textContent = signo;
+    document.getElementById("p3").textContent = numero2;
+}
+
 
 function limpiar() {
-    num1 = "";
-    num2 = "";
-    operador = "";
-    document.getElementById("pantalla").value = "";
+    numero1 = "";
+    numero2 = "";
+    signo = "";
+
+    mostrar();
 }
 
+
 function calcular() {
+
+    let n1 = Number(numero1);
+    let n2 = Number(numero2);
     let resultado = 0;
 
-    if (operador === "+") {
-        resultado = parseFloat(num1) + parseFloat(num2);
+    if (signo === "+") {
+        resultado = n1 + n2;
     }
-    if (operador === "-") {
-        resultado = parseFloat(num1) - parseFloat(num2);
+    else if (signo === "-") {
+        resultado = n1 - n2;
     }
-    if (operador === "*") {
-        resultado = parseFloat(num1) * parseFloat(num2);
+    else if (signo === "*") {
+        resultado = n1 * n2;
     }
-    if (operador === "/") {
-        resultado = parseFloat(num1) / parseFloat(num2);
+    else if (signo === "/") {
+        resultado = n1 / n2;
     }
 
-    document.getElementById("pantalla").value = resultado;
+    
+    numero1 = resultado;
+    numero2 = "";
+    signo = "";
+
+    mostrar();
 }
